7f25f4cde34f7d4e0cd06122c2abf5d7e1305326
tms-nguyenvu <vu.nguyen1@tomosia.com>
2025-04-23 14:43:58 +0700

    Add changes from feat/30-login-and-roles to new branch 1

--- sale-ai-v1/app/controllers/application_controller.rb
+++ sale-ai-v1/app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_url, alert: exception.message }
+      format.json { render json: { error: exception.message }, status: :forbidden }
+    end
+  end
+
+  protected
+    def configure_permitted_parameters
+      # devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
+    end
 end
