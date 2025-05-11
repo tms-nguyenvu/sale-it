e6bf8d6fed6ae5e7a55f6786225d47b63d3cdbb0
tms-nguyenvu <vu.nguyen1@tomosia.com>
2025-05-11 20:32:12 +0700

    fix: error donmain

--- app/controllers/admin/email_tracking_controller.rb
+++ app/controllers/admin/email_tracking_controller.rb
@@ -1,11 +1,18 @@
 class Admin::EmailTrackingController < ApplicationController
+  SAFE_DOMAINS = [
+    "https://tomosia.com.vn",
+  ].freeze
+
   def track_click
     email = Email.where(contact_id: params[:contact_id]).order(sent_at: :desc).first
+    email&.email_trackings&.create!(clicked_at: Time.current)
 
-    if email
-      email.email_trackings.create!(clicked_at: Time.current)
+    target_url = params[:target].to_s
+
+    if SAFE_DOMAINS.any? { |domain| target_url.start_with?(domain) }
+      redirect_to target_url, allow_other_host: true
+    else
+      redirect_to root_path, alert: "Invalid redirect target"
     end
-
-    redirect_to params[:target], allow_other_host: true
   end
 end
