877a1303eca3ddd24a2db576c04eb921626d2392
tms-nguyenvu <vu.nguyen1@tomosia.com>
2025-05-11 20:35:54 +0700

    fix: rubocop warning

--- app/controllers/admin/email_tracking_controller.rb
+++ app/controllers/admin/email_tracking_controller.rb
@@ -1,18 +1,21 @@
 class Admin::EmailTrackingController < ApplicationController
-  SAFE_DOMAINS = [
-    "https://tomosia.com.vn",
-  ].freeze
+  SAFE_DOMAINS = [ "tomosia.com.vn" ].freeze
+
+  def safe_redirect_url(target_url)
+    uri = URI.parse(target_url)
+    if uri.host.present? && SAFE_DOMAINS.include?(uri.host)
+      target_url
+    else
+      root_path
+    end
+  rescue URI::InvalidURIError
+    root_path
+  end
 
   def track_click
     email = Email.where(contact_id: params[:contact_id]).order(sent_at: :desc).first
     email&.email_trackings&.create!(clicked_at: Time.current)
 
-    target_url = params[:target].to_s
-
-    if SAFE_DOMAINS.any? { |domain| target_url.start_with?(domain) }
-      redirect_to target_url, allow_other_host: true
-    else
-      redirect_to root_path, alert: "Invalid redirect target"
-    end
+    redirect_to safe_redirect_url(params[:target])
   end
 end
