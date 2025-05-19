c2abd56565243dbbffeb13f5ac37ceda4ef7d0cb
tms-nguyenvu <vu.nguyen1@tomosia.com>
2025-05-19 15:55:49 +0700

    feat: update ux

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -43,5 +43,5 @@ class Lead < ApplicationRecord
   belongs_to :manager, class_name: "User", optional: true
   has_many :email_replies, dependent: :destroy
   has_many :emails, dependent: :nullify
-  has_many :proposals, dependent: :nullify
+  has_many :proposals, dependent: :destroy
 end
